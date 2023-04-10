558a458a39d6d3e5f17d67c313a07b780abdbf04
Arigaran <arigaran.sundaram@gamil.com>
2023-04-10 20:45:44 +0530

    bg-img

--- mini/src/App.js
+++ mini/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes,Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Navbar from './component/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
@@ -10,7 +10,7 @@ import Notfound from './pages/Notfound';
 import Appbar from './component/Appbar';
 function App() {
   return (
-    <div className="App">
+    <div className="App bgimg">
       <Appbar></Appbar>
       <BrowserRouter>
       <Navbar/>
--- mini/src/component/Appbar.js
+++ mini/src/component/Appbar.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Appbar() {
   return (
     <div className='bar'>
-        <img className="logo" src='image1.jpeg'alt='logo'></img>
-        <button className='btn btn-success b'>sign in</button>
-        <button className='btn btn-success bt'>log in</button>
+        <img className="log" src='https://cdn.pixabay.com/photo/2014/04/02/10/35/book-303927_1280.png'alt='logo'></img>
+        <h1 id='name'>LIBRARY</h1>
+        <button className='button b'>Sign in</button>
+        <button className='button'>Log in</button>
         
     </div>
   )
